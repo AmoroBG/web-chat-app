f8f2fcd38eaa6acac5bba5af130b293a96b64a35
Amoro <amorobg@gmail.com>
2022-09-24 01:59:53 -0700

    Embed chat engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
 
-
-
-
-function App() {
-  return (
-    <div>
-       <h1> Welcome to my Chat App</h1>
-    </div>
-  )
+export function App() {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='Ibn'
+			userSecret='123123'
+			projectID='21199fe9-1b86-44c2-bc61-81b6e84e3dcc'
+		/>
+	);
 }
-
-export default App
