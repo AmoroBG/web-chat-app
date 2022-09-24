be2477cb787b8abdc3b9b016c5db90fe3f26f54d
Amoro <amorobg@gmail.com>
2022-09-24 14:29:26 -0700

    Fix some typos"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-// import React from 'react';
-// import { ChatEngine } from 'react-chat-engine';
-
-// export function App() {
-// 	return (
-// 		<ChatEngine
-// 			height='100vh'
-// 			userName='Ibn'
-// 			userSecret='123123'
-// 			projectID='21199fe9-1b86-44c2-bc61-81b6e84e3dcc'
-// 		/>
-// 	);
-// }
 import React from 'react'
-import DirectMessaging from './DirectMessaging';
+import DirectMessaging from './components/DirectMessaging'
 import './App.css';
 
 
